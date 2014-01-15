0c9cb2bb53c22ec1ebb9ff0ad38995f8eb99ed08
woho <woho@woho.woho>
2014-01-16 00:48:36 +0100

    Format code, more error handling, default color

--- i3status.c
+++ i3status.c
@@ -6,7 +6,7 @@
 #include <libnotify/notify.h>
 
 #define COLOR_DARK "#555555"
-#define COLOR_DEFAULT "#dddd00"
+#define COLOR_DEFAULT NULL
 #define COLOR_CRITICAL "#ff0000"
 
 #define BATLOW 5
@@ -14,7 +14,8 @@
 
 static int is_battery_ok = 1;
 
-static void escape_quotes_nl(char *str) {
+static void escape_quotes_nl(char *str)
+{
     int i, k;
     char *tmp;
 
@@ -30,21 +31,38 @@ static void escape_quotes_nl(char *str) {
     free(tmp);
 }
 
-static void print_icon(const char *icon, const char *color, int nocomma) {
-    printf("{ \"separator_block_width\" : 0, \"separator\" : false, \"full_text\" : \"\", \"icon\" : \"/usr/share/icons/stlarch_icons/%s.xbm\", \"color\" : \"%s\", \"icon_color\" : \"%s\" }", icon, color, color);
-    if (! nocomma) {
+static void print_icon(const char *icon, const char *color, int nocomma)
+{
+    if (color) {
+        printf("{ \"separator_block_width\" : 0, \"separator\" : false, \"full_text\" : \"\", "
+               "\"icon\" : \"/usr/share/icons/stlarch_icons/%s.xbm\", \"color\" : \"%s\", "
+               "\"icon_color\" : \"%s\" }", icon, color, color);
+    } else {
+        printf("{ \"separator_block_width\" : 0, \"separator\" : false, \"full_text\" : \"\", "
+               "\"icon\" : \"/usr/share/icons/stlarch_icons/%s.xbm\""
+               " }", icon, color);
+    }
+
+    if (!nocomma) {
         putc(',', stdout);
     }
 }
 
-static void print_text(const char *text, const char *color, int nocomma) {
-    printf("{ \"separator\" : false, \"full_text\" : \"%s \", \"color\" : \"%s\" }", text, color);
-    if (! nocomma) {
+static void print_text(const char *text, const char *color, int nocomma)
+{
+    if (color) {
+        printf("{ \"separator\" : false, \"full_text\" : \"%s \", \"color\" : \"%s\" }", text, color);
+    } else {
+        printf("{ \"separator\" : false, \"full_text\" : \"%s \" }", text);
+    }
+
+    if (!nocomma) {
         putc(',', stdout);
     }
 }
 
-static void print_date() {
+static void print_date()
+{
     time_t now;
     char datebuf[32];
 
@@ -54,12 +72,16 @@ static void print_date() {
     print_text(datebuf, COLOR_DEFAULT, 1);
 }
 
-static void print_battery() {
+static void print_battery()
+{
     FILE *fp;
     int percentage, online;
     char batbuf[4];
 
     fp = fopen("/sys/class/power_supply/ADP1/online", "r");
+    if (!fp) {
+        return;
+    }
     fread(batbuf, 1, sizeof batbuf, fp);
     fclose(fp);
 
@@ -68,59 +90,64 @@ static void print_battery() {
     }
 
     fp = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+    if (!fp) {
+        return;
+    }
     fread(batbuf, 1, sizeof batbuf, fp);
     fclose(fp);
 
     percentage = atoi(batbuf);
+    if (percentage > 100) {
+        percentage = 100;
+    }
     snprintf(batbuf, sizeof batbuf, "%d", percentage);
 
     if (online) {
         print_icon("ac1", COLOR_DEFAULT, 0);
         print_text(batbuf, COLOR_DEFAULT, 0);
         is_battery_ok = 1;
-    }
-    else if (percentage <= BATLOW) {
+    } else if (percentage <= BATLOW) {
         print_icon("batt1empty", COLOR_CRITICAL, 0);
         print_text(batbuf, COLOR_CRITICAL, 0);
 
         if (is_battery_ok) {
-            NotifyNotification *msg = notify_notification_new("Battery low!", "Battery low!", "dialog-critical");
+            NotifyNotification *msg;
+            msg = notify_notification_new("Battery low!", "Battery low!", "dialog-critical");
             notify_notification_show(msg, NULL);
         }
 
         is_battery_ok = 0;
-    }
-    else if (percentage < BATFULL) {
+    } else if (percentage < BATFULL) {
         print_icon("batt1half", COLOR_DEFAULT, 0);
         print_text(batbuf, COLOR_DEFAULT, 0);
         is_battery_ok = 1;
-    }
-    else {
+    } else {
         print_icon("batt1full", COLOR_DEFAULT, 0);
         print_text(batbuf, COLOR_DEFAULT, 0);
         is_battery_ok = 1;
     }
 }
 
-static void print_mpd() {
+static void print_mpd()
+{
     char title[64];
     struct mpd_connection *conn;
     struct mpd_song *song;
     struct mpd_status *status;
 
     conn = mpd_connection_new(NULL, 0, 200);
-    if (! conn) {
+    if (!conn) {
         return;
     }
 
     song = mpd_run_current_song(conn);
-    if (! song) {
+    if (!song) {
         mpd_connection_free(conn);
         return;
     }
 
     status = mpd_run_status(conn);
-    if (! status) {
+    if (!status) {
         mpd_connection_free(conn);
         mpd_song_free(song);
         return;
@@ -148,7 +175,8 @@ static void print_mpd() {
     print_text(title, COLOR_DARK, 0);
 }
 
-static void print_alsa() {
+static void print_alsa()
+{
     snd_mixer_elem_t *elem;
     snd_mixer_t *handle;
     snd_mixer_selem_id_t *sid;
@@ -156,14 +184,29 @@ static void print_alsa() {
     int unmuted;
     char volbuf[8];
 
-    snd_mixer_open(&handle, 0);
-    snd_mixer_attach(handle, "default");
-    snd_mixer_selem_register(handle, NULL, NULL);
-    snd_mixer_load(handle);
+    if (snd_mixer_open(&handle, 0)) {
+        return;
+    }
+    if (snd_mixer_attach(handle, "default")) {
+        snd_mixer_close(handle);
+        return;
+    }
+    if (snd_mixer_selem_register(handle, NULL, NULL)) {
+        snd_mixer_close(handle);
+        return;
+    }
+    if (snd_mixer_load(handle)) {
+        snd_mixer_close(handle);
+        return;
+    }
     snd_mixer_selem_id_alloca(&sid);
     snd_mixer_selem_id_set_index(sid, 0);
     snd_mixer_selem_id_set_name(sid, "Master");
     elem = snd_mixer_find_selem(handle, sid);
+    if (!elem) {
+        snd_mixer_close(handle);
+        return;
+    }
     snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
     snd_mixer_selem_get_playback_volume(elem, 0, &volume);
     snd_mixer_selem_get_playback_switch(elem, SND_MIXER_SCHN_FRONT_LEFT, &unmuted);
@@ -175,17 +218,18 @@ static void print_alsa() {
     print_text(volbuf, COLOR_DEFAULT, 0);
 }
 
-static void print_line() {
+static void print_line()
+{
     print_mpd();
     print_alsa();
     print_battery();
     print_date();
 }
 
-static void sighandler(int signo) {
-}
+static void sighandler(int signo) { }
 
-int main() {
+int main()
+{
     notify_init("i3status");
     signal(SIGUSR1, &sighandler);
     printf("{\"version\":1}\n [");
@@ -194,8 +238,9 @@ int main() {
         print_line();
         printf("],");
         fflush(stdout);
-        sleep(1);
+        sleep(3);
     }
 
     return 0;
 }
+
